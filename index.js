534d840ef20411b9a9a5dd880e8173929cd217ec
harshdeep91 <harshdeepraghuwanshi91@gmail.com>
2023-02-13 14:37:17 +0530

    updated

--- index.js
+++ index.js
@@ -1,69 +1,40 @@
-import passwordChecker from "./passwordChecker.js";
-const imageData = null;
-const image = document.querySelector(".image");
+import passwordChecker from "/passwordChecker.js";
+const image = document.getElementById("image");
 image.addEventListener("change", (e) => {
   const files = document.getElementById("files").files;
-
   if (!files || files.length == 0) return;
   const file = files[0];
   const reader = new FileReader();
   reader.readAsDataURL(file);
   reader.onload = () => {
-    const changeImg = document.querySelector(".changeImg");
+    const changeImg = document.getElementById("changeImg");
     image.style.display = "none";
     changeImg.style.display = "flex";
-    const uploadedImage = changeImg.querySelector("img");
+    const uploadedImage = document.getElementById("uploadedImg");
     uploadedImage.setAttribute("src", reader.result);
   };
 });
 
-document.querySelector(".password").addEventListener("keyup", (e) => document.querySelector("#message").textContent=passwordChecker(e.target.value));
-const signUpForm = document.querySelector(".signUpForm");
+document.getElementById("password").addEventListener("keyup", (e) => document.getElementById("message").textContent=passwordChecker(e.target.value));
+const signUpForm = document.getElementById("signUpForm");
 
 signUpForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  const Name = document.querySelector(".name").value;
-  const Email = document.querySelector(".email").value;
-  const Password = document.querySelector(".password");
-  const confirmPassword = document.querySelector(".confirmPassword");
-  // alert(passwordChecker(Password.value));
+  const Password = document.getElementById("password");
+  const confirmPassword = document.getElementById("confirmPassword");
   if (Password.value != confirmPassword.value) {
     confirmPassword.value = "";
     alert("Password is not Matching");
   } else {
     if (passwordChecker(Password.value) == "Extremely difficult. ") {
       sendrequest();
-      // alert("Your sign up is saved");
     } else alert("Please Change Your Password");
   }
 });
-// const sendOtp= document.querySelector(".send");
-// console.log(sendOtp);
-// sendOtp.addEventListener("click",(e)=>{
-//   e.preventDefault();
-//   const otpInput=document.querySelector(".otpInput");
-//   otpInput.readOnly=false;
-//   otpInput.placeholder="OTP";
-
-//   // if(document.querySelector(".email").value==)
-//   let count=30;
-//   const otpCount= document.querySelector("#otp-count");
-//   console.log(otpCount);
-//   setInterval(()=>{
-//     otpCount.innerText=`Resend otp in ${count--} Seconds`;
-//     if(count==-1)
-//     clearInterval();
-//   },1000)
-
-// })
 
  async function sendrequest(){
   const formData = new FormData(signUpForm);
       formData.delete("confirmPassword");
-      // formData.delete("image");
-      // for(let item of formData)
-      // console.log(item[0],item[1]);
-      
        try{
         const response = await fetch("http://localhost:8090/api/signup",{
           method:"POST",
